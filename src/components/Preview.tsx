2d1f75cc4bd7aab5aaeb3b01889d021fddb68806
Baran Dogan <phscloq@gmail.com>
2023-06-26 10:13:25 +0300

    Skillsets added, style fixed to not match the height of form component

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -5,12 +5,12 @@ const allExperience= experience.map((experienceItem:any)=>{
                 return(
                     <div className="flex justify-between mt-4">
                         <div>
-                        <p className="font-bold text-xl">{experienceItem.company}</p>
-                        <p>{experienceItem.title}</p>
+                        <p className="font-bold text-xl">{experienceItem.company=='' ? 'Company': experienceItem.company}</p>
+                        <p>{experienceItem.title=='' ? 'Title': experienceItem.title}</p>
                         </div>
                         <div>
-                        <p>{experienceItem.location}</p>
-                        <p>{experienceItem.date}</p>
+                        <p>{experienceItem.location=='' ? 'Location': experienceItem.location}</p>
+                        <p>{experienceItem.date=='' ? 'dd/MM/YYYY': experienceItem.date}</p>
                         </div>
                     </div>
                 )
@@ -18,35 +18,35 @@ const allExperience= experience.map((experienceItem:any)=>{
 
 const allEducation= education.map((educationItem:any)=>{
     return(
-        <div className="flex justify-between">
+        <div className="flex justify-between  mt-4">
             <div>
-            <p className="font-bold text-xl">{educationItem.school}</p>
-            <p>{educationItem.deg}</p>
+            <p className="font-bold text-xl">{educationItem.school=='' ? 'School': educationItem.school}</p>
+            <p>{educationItem.deg=='' ? 'Degree': educationItem.deg}</p>
             </div>
             <div>
-            <p>{educationItem.location}</p>
-            <p>{educationItem.date}</p>
+            <p>{educationItem.location=='' ? 'Location': educationItem.location}</p>
+            <p>{educationItem.date=='' ? 'dd/MM/YYYY': educationItem.date}</p>
             </div>
         </div>
     )
 })
 const allSkills= skills.map((skillItem:any)=>{
     return(
-        <div>
+        <div className="border-0 border-solid border-b-2 border-b-zinc-900">
             <div>
-            <p>{skillItem.groupName}</p>
-            <p>{skillItem.deg}</p>
+            <p>{skillItem.groupName == '' ? 'Group Name:' : skillItem.groupName+ ': '}</p>
             </div>
-            <div>
-            <p>{skillItem.location}</p>
-            <p>{skillItem.date}</p>
+            <div className="flex flex-wrap gap-2">
+         {skillItem.skillSet.map((skill:any, index:any)=>{
+           return <p key={skillItem.groupName-index}>{skill==''? 'skill': skill}</p>
+         })}
             </div>
         </div>
     )
 })
 
     return (
-      <div className="max-w-3xl mx-auto mt-8 bg-white shadow-lg p-8">
+      <div className="shadow-custom w-2/4 my-24 mx-24 p-8 bg-stone-50 flex-1 h-fit">
         <div className="text-center">
             <div className=""> 
                 <h1 className="text-6xl">{personalInfo.name=='' ? 'Your Name': personalInfo.name}</h1>
@@ -59,7 +59,7 @@ const allSkills= skills.map((skillItem:any)=>{
         </div>
 
         <div className="mt-8">
-            <p className="text-sm">{summary.sum}</p>
+            <p className="text-sm">{summary.sum==''? 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi, perferendis voluptate omnis quae reprehenderit. Vel excepturi earum quasi quibusdam dolor quisquam, maiores enim exercitationem assumenda, natus ea officiis provident!': summary.sum}</p>
         </div>
 
         <div className="mt-10">
@@ -70,7 +70,12 @@ const allSkills= skills.map((skillItem:any)=>{
             <h1 className="text-2xl text-sky-600 font-semibold">Education</h1>
             {allEducation}
         </div>
-  
+        <div className="mt-10">
+        <h1 className="text-2xl text-sky-600 font-semibold">Skills</h1>
+
+        {allSkills}
+        </div>
+
   
       </div>
     );
